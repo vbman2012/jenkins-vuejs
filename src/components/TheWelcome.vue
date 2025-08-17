3f2ab7daa3be1dfb406139973ca0480eddf4f531
mahmoud alattar <vbman2012@live.com>
2025-08-17 12:41:30 +0300

    Update TheWelcome.vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -21,7 +21,7 @@ import WelcomeItem from './WelcomeItem.vue'
         <path d="M6 11.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
       </svg>
     </template>
-    <template #heading>Vue.js 3</template>
+    <template #heading>Vue.js 3 AppDev</template>
 
     This application is built with Vue.js 3, utilizing the Composition API and modern Vue features.
   </WelcomeItem>
