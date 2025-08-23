c2fa2e684b86baa3800c4862444069a05670e231
mahmoud alattar <vbman2012@live.com>
2025-08-23 15:38:38 +0300

    Update HelloWorld.vue
    update message to catch error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ msg }} - this is a message</h1>
     <h3>
       Welcome to our SaaS application ......
     </h3>
